3d9c7ff5d8bfe4be68d74bfde99d41d45f5d524b
Alexander Peras <128564378+AlexthePear@users.noreply.github.com>
2023-10-30 20:14:28 -0700

    WIP: Created form.svelte | Started Creating components for each question type

---
+++ src/lib/components/form.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import type { Form } from "../form/form.ts";
+    import type { QuestionList } from "../form/form.ts";
+    import type { Question } from "../form/form.ts";
+    import { QuestionType } from "../form/form.js";
+    
+    // Placeholder for now before we start importing actual data
+    /*
+    undo comments if you wanted to use this testing data.
+    let question_1 : Question = {
+        type: QuestionType.CHECKBOX,
+
+        name: "Day of week",
+
+        content: "What is the day of the week?",
+        
+        required: false
+    }
+    let question_2 : Question = {
+        type: QuestionType.TEXT,
+
+        name: "FRQ",
+
+        content: "How do you feel about this form?",
+
+        required: false
+    }
+
+    let questionList: QuestionList = {
+        data: [question_1, question_2],
+
+        shuffled: false
+    }
+
+    let currentForm: Form = {
+        id : "20",
+
+        questions: questionList
+    }
+    */
+</script>
+
+<!-- 
+    TO DO: 
+    Create Components for each question type.
+    create type script function to render the form to where each question is rendered based on its type. 
+    style the form
+
+-->
