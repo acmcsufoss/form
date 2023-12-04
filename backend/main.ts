c6e29ac7043ca4e979b190a56697d608e0ceb390
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-12-03 19:40:15 -0800

    add notes to `main.ts`

---
+++ backend/main.ts
@@ -0,0 +1,13 @@
+// TODO:
+// - Create backend that stores created/edited form data including Discord message ID and scheduled time(s) and target channel ID (and maybe target thread ID)
+// - Create endpoint that stores form response data.
+// - Sends webhooks to Discord to send messages at the scheduled time(s).
+// - Protect API endpoints with Discord OAuth2 authentication.
+// - Expose response data via API.
+// - Expose Discord interaction server for use with Discord buttons.
+//
+// References:
+// - https://discord.com/developers/docs/interactions/message-components#button-object-button-styles
+// - https://discord.com/developers/docs/interactions/message-components#buttons
+// - https://discord.com/developers/docs/topics/oauth2#authorization-code-grant-access-token-exchange-example
+//
