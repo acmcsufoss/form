52225085e78275e619987fd76b81faa1c381da3b
AlexthePear <alop5570@gmail.com>
2024-02-03 17:38:01 -0800

    wip

--- src/lib/components/availability_question_input.svelte
+++ src/lib/components/availability_question_input.svelte
@@ -20,7 +20,7 @@
 
 		type: QuestionType.AVAILABILITY,
 		
-		value: data.default || Array.from({ length: data.maxDateRanges }, () => [0, 0])
+		value: data.default || Array.from({ length: data.maxDateRanges || 1 }, () => [0, 0])
 	};
 </script>
 
