71e0b113927c8e064229e63931f964a98f6e3f1d
Alexander Peras <128564378+AlexthePear@users.noreply.github.com>
2024-02-09 04:42:49 -0800

    npm run format

--- src/lib/components/form.svelte
+++ src/lib/components/form.svelte
@@ -250,8 +250,8 @@
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
 	/* uncomment the global to see css components */
-	
-    /* :global(*) {
+
+	/* :global(*) {
         border-style: dotted;
         border-color: red;
     } */
