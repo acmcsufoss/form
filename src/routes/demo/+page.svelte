cb59a21154682b3fb834c7d84e365b987433c594
keillor <69380836+keillor@users.noreply.github.com>
2024-03-17 13:35:46 -0700

    showing the BooleanQuestionInputEditor in the demo page.

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
 	import DemoForm from './demo_form.svelte';
+	import BooleanQuestionInputEditor from '$lib/components/questions/boolean/boolean_question_input_editor.svelte';
+	import { QuestionType, type BooleanQuestion } from '$lib/form';
+	var data: BooleanQuestion = {
+		type: QuestionType.BOOLEAN,
+		name: 'internal name',
+		content: 'Are you 18 years old?',
+		required: true,
+		style: 'checkbox',
+		value: false
+	};
 </script>
 
+<BooleanQuestionInputEditor {...data} />
+<!-- svelte-ignore empty-block -->
+{#if data.value}
+	<p>the value is TRUE</p>
+{/if}
 <DemoForm />
