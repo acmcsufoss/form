d62847c371fe447abf9fa02be00ab396dbf48083
keillor <69380836+keillor@users.noreply.github.com>
2024-03-17 13:33:34 -0700

    created booleanQuestion InputEditor. Had to add the export keyword to a few existing components to allow binding of certain components.

---
+++ src/lib/components/questions/boolean/boolean_question_input_editor.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { QuestionType, type BooleanQuestion, type SelectQuestion } from '$lib/form';
+	import BooleanQuestionInput from './boolean_question_input.svelte';
+	import TextQuestionInput from '../text/text_question_input.svelte';
+	import TextareaQuestionInput from '../textarea/textarea_question_input.svelte';
+	import SelectQuestionInput from '../select/select_question_input.svelte';
+	var data = $$props as BooleanQuestion;
+	/* 
+    type: QuestionType.BOOLEAN,
+		name: 'internal name',
+		content: 'Are you 18 years old?',
+		required: false,
+		style: 'checkbox',
+		value: false */
+	var mySelect: SelectQuestion = {
+		type: QuestionType.SELECT,
+		options: [
+			{ content: 'checkbox', value: 'checkbox' },
+			{ content: 'radio', value: 'radio' }
+		],
+		name: 'booleanStyleSelector',
+		content: 'Choose A Style'
+	};
+</script>
+
+<TextQuestionInput value={data.content} content="Question Title" placeholder="Question Title" />
+<TextQuestionInput
+	content="Internal Question Name"
+	value={data.name}
+	placeholder="Internal Question Name"
+/>
+<SelectQuestionInput {...mySelect} />
+<BooleanQuestionInput value={data.required} content="Required" />
+<details>
+	<summary>Sample</summary>
+	<BooleanQuestionInput {data} />
+</details>
