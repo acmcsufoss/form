86a8bab202648b232d5487cb0ad588467032f4e6
keillor <69380836+keillor@users.noreply.github.com>
2024-04-11 00:08:55 -0700

    created datetime editor

---
+++ src/lib/components/questions/datetime/datetime_question_input_editor.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { DateQuestion, DatetimeQuestion } from '$lib/form';
+	import BaseEditor from '../base/base_editor.svelte';
+	import DatetimeQuestionInput from './datetime_question_input.svelte';
+	export let data = $$props as DatetimeQuestion;
+
+	/* type: QuestionType.DATE,
+				name: 'justDate',
+				content: 'Pick a date (jk you can only pick today)',
+				required: false,
+				value: new Date().toISOString(),
+				min: new Date().toISOString(),
+				max: new Date().toISOString() */
+</script>
+
+<BaseEditor bind:data>
+	<fieldset>
+		<legend>Min Date/Time</legend>
+		<input type="datetime-local" bind:value={data.min} />
+	</fieldset>
+	<fieldset>
+		<legend>Max Date/Time</legend>
+		<input type="datetime-local" bind:value={data.max} />
+	</fieldset>
+	<fieldset>
+		<legend>Default Date/Time</legend>
+		<input type="datetime-local" bind:value={data.value} />
+	</fieldset>
+	<details>
+		<summary>Sample</summary>
+		<div class="question">
+			<DatetimeQuestionInput bind:data />
+		</div>
+	</details>
+</BaseEditor>
