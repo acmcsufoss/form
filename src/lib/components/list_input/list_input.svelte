0b402b1484d2bc98f082fc46c37cf3f6c15ea590
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-04-11 17:45:11 -0700

    wip

--- src/lib/components/list_input/list_input.svelte
+++ src/lib/components/list_input/list_input.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-	import type { ItemProps, Components } from './list_input.ts';
+	import { onMount } from 'svelte';
+	import type { ItemProps, Components } from './list_input';
+	import { initSortable } from './sortable';
 
 	export let components: Components;
 	export let value: ItemProps[] = [];
+
+	let ref: HTMLElement;
 
 	function deleteItem(i: number): void {
 		value = value.filter((_, index) => index !== i);
@@ -12,19 +16,22 @@
 		value = [...value, item];
 	}
 
-	// TODO: Make sortable items.
+	onMount(() => {
+		if (ref) {
+			initSortable(ref);
+		}
+	});
 </script>
 
-<div>
-	{#each value as item, i}
-		<div>
-			<svelte:component this={components.item} {...item} />
-			<svelte:component this={components.deleteItem} deleteAction={() => deleteItem(i)} />
-		</div>
-	{:else}
-		{#if components.emptySection}
-			<svelte:component this={components.emptySection} />
-		{/if}
-	{/each}
-	<svelte:component this={components.addItem} addAction={(item) => addItem(item)} />
-</div>
+{#if value.length > 0}
+	<ul bind:this={ref}>
+		{#each value as item, i}
+			<li>
+				<svelte:component this={components.item} {...item} />
+				<svelte:component this={components.deleteItem} deleteAction={() => deleteItem(i)} />
+			</li>
+		{/each}
+	</ul>
+{:else if components.emptySection}
+	<svelte:component this={components.emptySection} />
+{/if}
