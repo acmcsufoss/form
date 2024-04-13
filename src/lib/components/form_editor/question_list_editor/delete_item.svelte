73e300ab6e521e8d820d23c596049817d3c44daa
AlexthePear <alop5570@gmail.com>
2024-04-13 00:20:08 -0700

    left a note for delete_item.svelte

--- src/lib/components/form_editor/question_list_editor/delete_item.svelte
+++ src/lib/components/form_editor/question_list_editor/delete_item.svelte
@@ -4,4 +4,7 @@
 	export let deleteAction: DeleteItemProps['deleteAction'];
 </script>
 
+<!-- TODO: stop this button from firing when the user hits the enter key -->
+<!-- With the enter key still inactive its very easy to accidentally delete a question. -->
+<!-- Some inputs may also use an enter such as text area if the user writes mutliple paragraphs -->
 <button on:click={() => deleteAction()}>Delete</button>
