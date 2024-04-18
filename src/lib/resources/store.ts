3d19fc9f3361b4853343f0dff3afd8744bd79398
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-04-17 20:31:58 -0700

    cringe

--- src/lib/resources/store.ts
+++ src/lib/resources/store.ts
@@ -2,7 +2,7 @@ import { openKv } from '@deno/kv';
 import { KvStore } from '$lib/db/kv';
 import { DENO_KV_ACCESS_TOKEN, DENO_KV_CONNECT_URL } from '$env/static/private';
 
-const kvPath = DENO_KV_CONNECT_URL || './.denokv';
+const kvPath = DENO_KV_CONNECT_URL || './db.kv';
 const kv = await openKv(kvPath, { accessToken: DENO_KV_ACCESS_TOKEN });
 
 export const s = new KvStore(kv);
