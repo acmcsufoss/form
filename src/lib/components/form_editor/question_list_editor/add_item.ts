28af91a2af30b59bf485f055422ef87363aeaf9b
AlexthePear <alop5570@gmail.com>
2025-02-03 13:38:57 -0800

    Updated MakeBlankQuestion to make sure all required properties are set for different question types

--- src/lib/components/form_editor/question_list_editor/add_item.ts
+++ src/lib/components/form_editor/question_list_editor/add_item.ts
@@ -1,10 +1,27 @@
-import type { QuestionBase, QuestionType } from '$lib/form';
+import { QuestionType } from '$lib/form';
+import type { Question } from '$lib/form/form';
 
-export function makeBlankQuestion(type: QuestionType): QuestionBase {
-	return {
-		type,
-		name: '',
-		content: 'No content.',
-		required: false
-	};
+export function makeBlankQuestion(type: QuestionType): Question {
+	switch (type) {
+		case QuestionType.RADIO_GROUP:
+			return {
+				type,
+				name: '',
+				content: 'No content.',
+				choices: []
+			};
+		case QuestionType.SELECT:
+			return {
+				type,
+				name: '',
+				content: 'No content.',
+				options: []
+			};
+		default:
+			return {
+				type,
+				name: '',
+				content: 'No content.'
+			};
+	}
 }
