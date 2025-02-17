0c484bfdd65c0a7301b33ab54711d33f1b5383f2
AlexthePear <alop5570@gmail.com>
2025-02-16 21:22:18 -0800

    Fixed issue where hovering over "Create a form" created a new form, Now does action on click.

--- src/routes/forms/+page.svelte
+++ src/routes/forms/+page.svelte
@@ -4,10 +4,14 @@
 	import FormPreviewList from './form_preview_list.svelte';
 
 	export let data: PageData;
+
+	function handleCreateForm() {
+		window.location.href = makeFormNewURL();
+	}
 </script>
 
 <h1>Forms</h1>
 
 <FormPreviewList data={data.forms} />
 
-<a href={makeFormNewURL()}>Create a form</a>
+<button on:click={handleCreateForm}>Create a form</button>
